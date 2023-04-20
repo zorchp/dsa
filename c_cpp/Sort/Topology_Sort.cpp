c8149f188ae205438b0648c7f54da7d688735ccd
zorchp <zorchp@gmail.com>
2023-04-20 20:54:58 +0800

    add-topological_sort

--- c_cpp/Sort/Topology_Sort.cpp
+++ c_cpp/Sort/Topology_Sort.cpp
@@ -1,58 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 using namespace std;
- 
-bool TopSort(vector<vector<int>> &G, int n, vector<int> &inDegree) {
-    /*
-    *   param
-    *   G： 邻接表
-    *   n： 顶点数
-    *   InDegree：  记录顶点的入度  
-    */
-    int num = 0;                //记录加入拓扑排序的顶点数
-    queue<int> q;
-    for (int i = 0; i < n; i++)
-        if (inDegree[i] == 0)
-            q.push(i);      //将所有入度为0的顶点入队
-    while (!q.empty()) {
-        int u = q.front();      //取队首顶点u
-        cout << u << " ";       
-        q.pop();
-        for (int i = 0; i < G[u].size(); i++) {
-            int v = G[u][i];        //u的后继节点
-            inDegree[v]--;          //v的入度减1
-            if (inDegree[v] == 0)       //顶点v的入度减为0则入队
-                q.push(v);
-        }
-        G[u].clear();           //清空顶点u的所有出边
-        num++;
-    }
-    if (num == n)               //加入拓扑序列的顶点数为n，说明拓扑排序成功，否则，失败
-        return true;
-    else
-        return false;
+
+
+ostream& operator<<(ostream& os, const vector<int>& v) {
+    for (auto i : v) os << i << " ";
+    return os << endl;
 }
- 
+ostream& operator<<(ostream& os, queue<int> v) { // pass by value
+    for (; !v.empty(); v.pop()) os << v.front() << " ";
+    return os << endl;
+}
+ostream& operator<<(ostream& os, stack<int> v) { // pass by value
+    for (; !v.empty(); v.pop()) os << v.top() << " ";
+    return os << endl;
+}
+
+
+// 图的例子: 参见
+// https://oi-wiki.org/graph/images/topo-example.svg
+// 邻接表
+vector<vector<int>> G{
+    {1, 5, 6}, {},  {0, 3},       {5}, {},   {4}, {4, 9},
+    {6},       {7}, {10, 11, 12}, {},  {12}, {},
+};
+
+int n = G.size(); // n: vertex
+// int m{15};        // m: edge
+// 存储结点的入度
+vector<int> in(n);
+//{1, 1, 0, 1, 2, 2, 2, 1, 0, 1, 1, 1, 2};
+
+bool topoSort_Kahn() {
+    vector<int> L;
+    queue<int> S;
+    // 计算节点入度
+    for (auto v : G)
+        for (int i : v) ++in[i];
+    // cout << in;
+    for (int i{}; i < n; ++i)
+        if (in[i] == 0) S.push(i); // 存入度为 0 的节点
+    // cout << S;
+    while (!S.empty()) {
+        int u = S.front();
+        S.pop();
+        L.push_back(u);
+        // cout << G[u];
+        for (auto v : G[u]) {
+            if (--in[v] == 0) S.push(v);
+        }
+    }
+    // cout << L.size() << endl;
+    if (L.size() == n) {
+        cout << "排序结果:\n" << L;
+        return true;
+    }
+    return false;
+}
+
+void topoSort_DFS() {
+    //
+    bool visited[n];
+    memset(visited, false, sizeof(visited));
+    stack<int> st;
+
+    function<void(int)> dfs = [&](int v) {
+        visited[v] = true;
+        for (auto i : G[v])
+            if (!visited[i]) dfs(i);
+        st.emplace(v);
+    };
+    for (int i{}; i < n; ++i)
+        if (!visited[i]) dfs(i);
+    cout << st;
+}
+
 int main() {
-    int n, m;
-    cout << "请输入顶点数和边数:";
-    cin >> n >> m;
-    vector<vector<int>> G(n);
-    for (int i = 0; i < m; i++) {
-        int x, y;
-        cout << "请输入第" << i+1 << "条边的顶点:";
-        cin >> x >> y;
-        G[x].push_back(y);
-    }
-    cout << "拓扑排序为:";
-    vector<int> inDegree(n);
-    for (auto x : G) {
-        for (auto y : x)
-            inDegree[y]++;
-    }
-    bool res = TopSort(G, n, inDegree);
- 
-    return 0;
-    
+    // topoSort_Kahn(); // BFS
+    // 2 8 0 3 7 1 5 6 4 9 10 11 12
+    topoSort_DFS();
+    // 8 7 2 3 0 6 9 11 12 10 5 4 1
 }
